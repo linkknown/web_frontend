596a7623cd9a0bc6ea4258809b92b6f162c71ab2
389093982 <389093982@qq.com>
2020-10-02 15:54:33 +0800

    示例代码优化

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,47 @@ $(".modalDiv > button").click(function () {
     // 打开模态对话框后禁用滚动条
     $("body").css("overflow", "hidden");
 });
+
+
+// 当前选中的索引
+var index = 0;
+// 除第一张其余的图片全部隐藏
+$(".swiperList > li:not(:eq(0))").hide();
+// 定义页码
+var totalNum = $(".swiperList > li").length;
+var html_page = "";
+for (var i=1; i<= totalNum; i++) {
+    if (i === 1) {
+        html_page += "<li class='active'>" + i + "</li>";
+    } else {
+        html_page += "<li>" + i + "</li>";
+    }
+}
+$(".swiperPage").html(html_page);
+$(".swiperPage > li").click(function () {
+    index = $(this).index();
+    showPic(index);
+});
+
+function showPic (index) {
+    $(".swiperList > li:eq(" + index + ")").show().siblings("li").hide();
+    $(".swiperPage > li:eq(" + index + ")").addClass("active").siblings("li").removeClass("active");
+}
+
+// .hover( handlerIn, handlerOut )
+$(".swiperList").hover(function () {
+    clearInterval(window.swiper_timer);
+}, function () {
+    window.swiper_timer = setInterval(function () {
+        showPic(index);
+        index ++;
+        if (index === totalNum) {
+            index = 0;
+        }
+    }, 2000);
+}).trigger("mouseleave");   // 触发离开使其能自动播放
+
+
 $(".dialogClose").click(function () {
     $(".modalShade").addClass('hide');
     $(".modalDialog").addClass('hide')
